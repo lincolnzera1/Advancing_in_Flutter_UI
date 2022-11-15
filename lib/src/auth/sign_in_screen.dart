63bfcdd7cb5830c0af4d5f4fbd99b1259201d5b0
lincolnzera1 <88245209+lincolnzera1@users.noreply.github.com>
2022-11-15 13:24:12 -0300

    Learning text animation, and text.rich configs

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:getx_redemption/src/auth/components/custom_text_field.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class SignInScreen extends StatelessWidget {
   const SignInScreen({super.key});
@@ -15,7 +16,49 @@ class SignInScreen extends StatelessWidget {
           Expanded(
               child: Container(
             color: Colors.green,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Nome do App
+                  const Text.rich(
+                      TextSpan(style: TextStyle(fontSize: 50), children: [
+                    TextSpan(
+                        text: "Lit",
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold)),
+                    TextSpan(
+                        text: "Fruit",
+                        style: TextStyle(
+                            color: Colors.redAccent,
+                            fontWeight: FontWeight.bold)),
+                  ])),
+
+                  // Categorias
+                  SizedBox(
+                    height: 30,
+                    child: DefaultTextStyle(
+                      style: const TextStyle(fontSize: 25),
+                      child: AnimatedTextKit(
+                        pause: Duration.zero,
+                        animatedTexts: [                          
+                          FadeAnimatedText("Frutas"),
+                          FadeAnimatedText("Verduras"),
+                          FadeAnimatedText("Legumes"),
+                          FadeAnimatedText("Carnes"),
+                          FadeAnimatedText("Cereais"),
+                          FadeAnimatedText("Laticíneos"),
+                        ],
+                        repeatForever: true,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
           )),
+
+          // Login / Criar conta
           Container(
             padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
             decoration: const BoxDecoration(
@@ -37,6 +80,7 @@ class SignInScreen extends StatelessWidget {
                   isSecret: true,
                 ),
 
+                // Entrar
                 SizedBox(
                   height: 50,
                   child: ElevatedButton(
@@ -49,6 +93,8 @@ class SignInScreen extends StatelessWidget {
                         style: TextStyle(fontSize: 18),
                       )),
                 ),
+
+                // Esqueceu a senha?
                 Padding(
                   padding: const EdgeInsets.symmetric(vertical: 5),
                   child: Align(
@@ -59,6 +105,8 @@ class SignInScreen extends StatelessWidget {
                             style: TextStyle(color: Colors.red))),
                   ),
                 ),
+
+                // Linhas e Ou
                 Padding(
                   padding: EdgeInsets.only(bottom: 10),
                   child: Row(
@@ -80,17 +128,21 @@ class SignInScreen extends StatelessWidget {
                     ],
                   ),
                 ),
+
+                // Criar conta
                 SizedBox(
                   height: 50,
                   child: OutlinedButton(
                       style: OutlinedButton.styleFrom(
                         shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(18)
-                        ),
+                            borderRadius: BorderRadius.circular(18)),
                         side: const BorderSide(width: 3, color: Colors.green),
                       ),
                       onPressed: () {},
-                      child: const Text("Criar conta", style: TextStyle(fontSize: 18),)),
+                      child: const Text(
+                        "Criar conta",
+                        style: TextStyle(fontSize: 18),
+                      )),
                 )
               ],
             ),
